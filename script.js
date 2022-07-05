340095e1685c88fe7a3cd09ef1caa8a77d711f86
kipraschi <kipraschi@gmail.com>
2022-07-05 23:53:00 +0300

    Add playRound function that plays a game once and returns who won

--- script.js
+++ script.js
@@ -15,4 +15,37 @@ switch (random) {
 return object;
 }
 
-console.log(computerPlay());
+
+function playRound(playerSelection, computerSelection){
+    // rock > scissors
+    if (playerSelection.toLowerCase() == `rock` && 
+        computerSelection.toLowerCase() == `scissors`)
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    if (playerSelection.toLowerCase() == `scissors` && 
+        computerSelection.toLowerCase() == `rock`)
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    // paper > rock
+    if (playerSelection.toLowerCase() == `paper` && 
+        computerSelection.toLowerCase() == `rock`)
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    if (playerSelection.toLowerCase() == `rock` && 
+        computerSelection.toLowerCase() == `paper`)
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    // scissors > paper
+    if (playerSelection.toLowerCase() == `scissors` && 
+        computerSelection.toLowerCase() == `paper`)
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    if (playerSelection.toLowerCase() == `paper` && 
+        computerSelection.toLowerCase() == `scissors`)
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+
+    else
+        return `It's a tie. Play another round`;
+}
+
+const playerSelection = 'Rock';
+const computerSelection = computerPlay();
+
+console.log(`Player: ${playerSelection}`); //debug
+console.log(`Computer: ${computerSelection}`); //debug
+console.log(playRound(playerSelection, computerSelection));
