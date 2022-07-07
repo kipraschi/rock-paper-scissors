9f04f05f4fe59db6d036b018a7365df8d88a0df0
kipraschi <kipraschi@gmail.com>
2022-07-07 19:24:23 +0300

    Fix computerScore bug if a paper is selected by the computer

--- script.js
+++ script.js
@@ -4,24 +4,24 @@ var computerScore = 0;
 game();
 
 function game() {
-	for (let i = 0; i < 5; i++) {
-		var playerSelection = prompt(`Game ${i+1}/5: Rock, Paper, Scissors?`);
+	for (let i = 1; i <= 5; i++) {
+		let playerSelection = prompt(`Game ${i}/5: Rock, Paper, Scissors?`);
 		if (
 			playerSelection.toLowerCase() != `rock` &&
 			playerSelection.toLowerCase() != `paper` &&
 			playerSelection.toLowerCase() != `scissors`
 		)
 			alert(`Choose Rock, Paper or Scissors next time! Computer wins this round.`);
-		var computerSelection = computerPlay();
+		let computerSelection = computerPlay();
 		playRound(playerSelection, computerSelection);
-        
-		console.log(`Game ${i + 1}:`);
+
+		console.log(`Game ${i}:`);
 		console.log(`Player: ${playerSelection}`);
 		console.log(`Computer: ${computerSelection}`);
-        console.log(`Player ${playerScore}:${computerScore} Computer \n\n`);
+		console.log(`Player ${playerScore}:${computerScore} Computer \n\n`);
 	}
 	console.log(findWinner());
-    alert(findWinner());
+	alert(findWinner());
 }
 
 function computerPlay() {
@@ -35,26 +35,25 @@ function computerPlay() {
 	return object;
 }
 
-//playRound() increases global score of the winner
 function playRound(playerSelection, computerSelection) {
 	// rock > scissors
 	if (playerSelection.toLowerCase() == `rock` && computerSelection.toLowerCase() == `scissors`)
-		playerScore++;
+		return playerScore++;
 	if (playerSelection.toLowerCase() == `scissors` && computerSelection.toLowerCase() == `rock`)
-		computerScore++;
+		return computerScore++;
 	// paper > rock
 	if (playerSelection.toLowerCase() == `paper` && computerSelection.toLowerCase() == `rock`)
-		playerScore++;
+		return playerScore++;
 	if (playerSelection.toLowerCase() == `rock` && computerSelection.toLowerCase() == `paper`)
-		computerScore++;
+		return computerScore++;
 	// scissors > paper
 	if (playerSelection.toLowerCase() == `scissors` && computerSelection.toLowerCase() == `paper`)
-		playerScore++;
+		return playerScore++;
 	if (playerSelection.toLowerCase() == `paper` && computerSelection.toLowerCase() == `scissors`)
-		computerScore++;
+		return computerScore++;
 	//a tie keeps doesn't change the scores,
 	//but if a player enters something else than the given selections computer gains a point
-	else if (playerSelection.toLowerCase() != computerSelection.toLowerCase()) computerScore++;
+	else if (playerSelection.toLowerCase() != computerSelection.toLowerCase()) return computerScore++;
 }
 
 function findWinner() {
