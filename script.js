00acfd2a52c050909eca994bfb0ff8cc00e8257e
kipraschi <kipraschi@gmail.com>
2022-07-31 21:14:06 +0300

    make game logic work with the new UI

--- script.js
+++ script.js
@@ -1,54 +1,49 @@
 let playerScore = 0;
 let computerScore = 0;
 
-let resultsBoard = document.querySelector("#results");
-let buttons = document.querySelectorAll("button");
-// let roundInfo = document.querySelector("#roundInfo");
-
-let scoreDisplay = document.createElement("h2");
-let playerChoice = document.createElement("h2");
-let computerChoice = document.createElement("h2");
-
-
-// scoreDisplay.classList.add("score");
-resultsBoard.appendChild(playerChoice);
-resultsBoard.appendChild(scoreDisplay);
-resultsBoard.appendChild(computerChoice);
-
-
-buttons.forEach((button) => {
-	button.addEventListener("click", () => game(button.id));
-});
-
+document.querySelector("#underline").textContent = `Choose your weapon!`;
 newGame();
 
 function newGame() {
-	scoreDisplay.textContent = "Rock, Paper, or Scissors?";
-	playerChoice.textContent = "";
-	computerChoice.textContent = "";
 	playerScore = computerScore = 0;
 }
 
-function game(playerSelection) {
-	if (
-		playerSelection != `Rock` &&
-		playerSelection != `Paper` &&
-		playerSelection != `Scissors`
-	)
-		alert(
-			`Choose Rock, Paper or Scissors next time! Computer wins this round.`
-		);
+let playerChoice = document.querySelector("#playerSelection");
+let computerChoice = document.querySelector("#computerSelection");
+let playerScoreDisplay = document.querySelector(".playerScore");
+let computerScoreDisplay = document.querySelector(".computerScore");
+// let playerRound = document.createElement("div");
+// let computerRound = document.createElement("div");
+
+let buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+	button.addEventListener("click", () => {
+		playRound(button.id);
+	});
+});
+
+function playRound(playerSelection) {
+	buttons.forEach((button) => {
+		button.removeAttribute("class", "clicked");
+	});
+
 	let computerSelection = computerPlay();
-	playRound(playerSelection, computerSelection);
 
-	playerChoice.textContent = `Player: ${playerSelection}`;
-	computerChoice.textContent = ` Computer: ${computerSelection}`;
-	scoreDisplay.textContent = `${playerScore} : ${computerScore}`;
+	if (
+		(playerSelection == `Rock` && computerSelection == `Scissors`) ||
+		(playerSelection == `Paper` && computerSelection == `Rock`) ||
+		(playerSelection == `Scissors` && computerSelection == `Paper`)
+	)
+		playerScore++;
+	if (
+		(playerSelection == `Scissors` && computerSelection == `Rock`) ||
+		(playerSelection == `Rock` && computerSelection == `Paper`) ||
+		(playerSelection == `Paper` && computerSelection == `Scissors`)
+	)
+		computerScore++;
 
-	if (playerScore == 5 || computerScore == 5) {
-		newGame();
-		scoreDisplay.textContent = `${findWinner()}`;
-	}
+	updatePage(playerSelection, computerSelection);
 }
 
 function computerPlay() {
@@ -59,31 +54,42 @@ function computerPlay() {
 	else if (random == 3) return "Scissors";
 }
 
-function playRound(playerSelection, computerSelection) {
-	if (
-		(playerSelection.toLowerCase() == `rock` &&
-			computerSelection.toLowerCase() == `scissors`) ||
-		(playerSelection.toLowerCase() == `paper` &&
-			computerSelection.toLowerCase() == `rock`) ||
-		(playerSelection.toLowerCase() == `scissors` &&
-			computerSelection.toLowerCase() == `paper`)
-	)
-		return playerScore++;
-	if (
-		(playerSelection.toLowerCase() == `scissors` &&
-			computerSelection.toLowerCase() == `rock`) ||
-		(playerSelection.toLowerCase() == `rock` &&
-			computerSelection.toLowerCase() == `paper`) ||
-		(playerSelection.toLowerCase() == `paper` &&
-			computerSelection.toLowerCase() == `scissors`)
-	)
-		return computerScore++;
-	// computer gains a point when the player choice is neither rock, paper
-	else if (playerSelection.toLowerCase() != computerSelection.toLowerCase())
-		return computerScore++;
+function updatePage(playerSelection, computerSelection) {
+	playerChoice.src = getIconSrc(playerSelection);
+	computerChoice.src = getIconSrc(computerSelection);
+
+	document
+		.querySelector(`#${playerSelection}`)
+		.setAttribute("class", "clicked");
+
+	displayScore();
+
+	if (isGameOver()) {
+		document.querySelector("#underline").textContent = `${findWinner()}`;
+		newGame();
+	} else
+		document.querySelector("#underline").textContent = `Choose your weapon!`;
+}
+
+function displayScore() {
+	playerScoreDisplay.textContent = `${playerScore}`;
+	computerScoreDisplay.textContent = `${computerScore}`;
+}
+
+function getIconSrc(selection) {
+	if (selection == `Rock`) return "./img/rock.svg";
+	if (selection == `Paper`) return "./img/paper.svg";
+	if (selection == `Scissors`) return "./img/scissors.svg";
+}
+
+function isGameOver() {
+	return playerScore == 5 || computerScore == 5;
 }
 
 function findWinner() {
 	if (playerScore > computerScore) return `You won!`;
 	else return `You lose!`;
 }
+
+// make the computer choice come in later
+// add a modal with the result and 'playAgain' button
